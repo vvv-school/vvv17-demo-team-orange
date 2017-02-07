f4d58fff0d30b3ea3bbc47cd0c0979c5ee5aba80
gian maria marconi <gmmarconi@gmail.com>
2017-02-07 19:19:26 +0100

    [movePlanner] Fixed CMakeLists and folder structure

--- src/movePlanner/src/movePlanner.cpp
+++ src/movePlanner/src/movePlanner.cpp
@@ -1,4 +1,5 @@
 #include <yarp/os/ConstString.h>
+#include <yarp/os/LogStream.h>
 #include "movePlanner.h"
 
 using namespace std;
@@ -15,25 +16,36 @@ bool movePlanner::updateModule()
 }
 bool movePlanner::configure(yarp::os::ResourceFinder &rf)
 {
-    yInfo()<<"Configuring move planner..."
+    yInfo()<<"Configuring move planner...";
+    //implement port opening and attach
+    if (!RPCPort.open("/movePlanner/rpc:i"))
+    {
+        yError()<<"Cannot open port";
+        return false;
+    }
+     
+    if (!attach(RPCPort))
+        return false;
 
     return true;
 }
 bool movePlanner::attach(yarp::os::RpcServer &source)
 {
-  return this->yarp().attachAsServer(source);
+  return yarp().attachAsServer(source);
 }
 bool movePlanner::interruptModule()
 {
-    if (port.asPort().isOpen()) port.interrupt();
+    if (RPCPort.asPort().isOpen()) RPCPort.interrupt();
     printf("Closing Server\n");
     return true;
 }
 bool movePlanner::close()
 {
      printf("Calling close function\n");
-    if (sManager.isRunning()) sManager.askToStop();
-    if (rManager.isRunning()) rManager.stop();
-    if (port.asPort().isOpen()) port.close();
+    if (RPCPort.asPort().isOpen()) RPCPort.close();
     return true;
+}
+yarp::sig::Vector movePlanner::computeNextMove(yarp::sig::Matrix state)
+{
+    
 }
