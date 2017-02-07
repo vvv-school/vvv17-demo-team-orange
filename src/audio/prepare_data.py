dd50d0e766930f3731e4458f7a21bdf27874eb97
EgorLakomkin <egor.lakomkin@gmail.com>
2017-02-07 10:19:46 +0100

    workign sound grabbing

--- src/audio/prepare_data.py
+++ src/audio/prepare_data.py
@@ -1,10 +1,10 @@
-from tempfile import NamedTemporaryFile
 import datetime
-import librosa
 from collections import defaultdict
 import soundfile
 import numpy as np
+from librosa.feature import melspectrogram
 from path import Path
+import os
 
 def load_annotations( file ):
     res = []
@@ -20,14 +20,14 @@ def load_annotations( file ):
                 } )
         return res
 
-def get_sec(ts):
-    return 60*60*ts.hour + 60*ts.minute + ts.second
+def get_msec(ts):
+    return 60 * 60 * ts.hour* 1000 + 60*ts.minute*1000 + ts.second * 1000 + ts.microsecond / 1000
 
-def check_if_timing_in_annotations( timing, annotations, threshold = 0.5 ):
-    timing_second = get_sec(timing)
+def check_if_timing_in_annotations( timing, annotations, threshold = 15 ):
+    timing_second = get_msec(timing)
     for ann in annotations:
-        ann_start = get_sec(ann["start_ts"])
-        ann_end = get_sec(ann["end_ts"])
+        ann_start = get_msec(ann["start_ts"])
+        ann_end = get_msec(ann["end_ts"])
         if timing_second > ann_start - threshold and timing_second < ann_end + threshold:
             return True
     return False
@@ -59,42 +59,36 @@ def create_temporal_dataset( directory ):
     """
     annotations_by_wav_file = defaultdict(list)
     for f in Path(directory).walkfiles("*.txt"):
-        annotations =
+        filename = str(f)
+        annotations = load_annotations( filename )
+        wav_file = os.path.abspath( filename[:-4]+".wav" )
         for ann in annotations:
-            annotations_by_url[ ann["wav_file"] ].append( ann )
+            annotations_by_wav_file[ wav_file ].append( ann )
 
     X, Y = [], []
+    for audio_file, lst_annotations in annotations_by_wav_file.items():
+        sequence_X = []
+        sequence_Y = []
+        spectrogram = spectrogram_from_file_librosa(audio_file, step=15,
+                                                    window = 25, max_freq = 8000)
 
-    for audio_file, lst_annotations in annotations_by_url.items():
-        with NamedTemporaryFile(suffix=".wav") as tmp_audio_file:
-            audio_file_name = tmp_audio_file.name
+        initial_timing = datetime.datetime( hour = 0, minute = 0, second  = 0,
+                                            year = 2016, month = 1, day = 1 )
+        timings = [initial_timing + datetime.timedelta( milliseconds = 15 * i )
+                   for i in range(spectrogram.shape[0]) ]
+        features = [{"feature" : f, "timing" : t }
+                    for f, t in zip( spectrogram, timings )]
+        for data_struct in features:
+            mfcc = data_struct["feature"]
+            timing = data_struct["timing"]
 
-            #pass
-            sequence_X = []
-            sequence_Y = []
-
-
-            #features = extract_temporal_mfcc_features(converted_audio_file)
-            spectrogram = spectrogram_from_file_librosa(converted_audio_file, step=50, window=50, max_freq=8000)
-
-            #just dummy date
-            initial_timing = datetime.datetime( hour = 0, minute = 0, second  = 0,
-                                                year = 2016, month = 1, day = 1 )
-            timings = [initial_timing + datetime.timedelta( milliseconds = 50 * i )
-                       for i in range(spectrogram.shape[0]) ]
-            features = [{"feature" : f, "timing" : t }
-                        for f, t in zip( spectrogram, timings )]
-            for data_struct in features:
-                mfcc = data_struct["feature"]
-                timing = data_struct["timing"]
-
-                sequence_X.append(mfcc)
-                if check_if_timing_in_annotations(timing, lst_annotations, threshold=0.3):
-                    sequence_Y.append(1)
-                else:
-                    sequence_Y.append(0)
-            X.append( sequence_X )
-            Y.append( sequence_Y )
+            sequence_X.append(mfcc)
+            if check_if_timing_in_annotations(timing, lst_annotations, threshold = 10):
+                sequence_Y.append(1)
+            else:
+                sequence_Y.append(0)
+        X.append( sequence_X )
+        Y.append( sequence_Y )
 
     return X,Y
 
@@ -103,4 +97,5 @@ def get_data( wav_file, annotation_file ):
     pass
 
 if __name__ == "__main__":
-    print( load_annotations( "./data/file_2.txt" ) )
+    #print( load_annotations( "./data/file_2.txt" ) )
+    X, Y = create_temporal_dataset( "./data" )
--- src/audio/speech_client.py
+++ src/audio/speech_client.py
@@ -1,31 +1,52 @@
 from __future__ import print_function
+import os
 import sys
+sys.path.append("/home/egor/robot-install/lib/python2.7/dist-packages")
 import yarp
+import numpy as np
+import librosa
 
 yarp.Network.init()
 
-#rf = yarp.ResourceFinder()
-#rf.setVerbose(True)
-#rf.setDefaultContext("myContext")
-#rf.setDefaultConfigFile("default.ini")
 
 p = yarp.BufferedPortSound()
-#p.open(10)
 p.open("/audio/in")
+
 
 if not yarp.NetworkBase_connect("/grabber/audio", p.getName()):
     print("cannot connect to audio port")
     sys.exit(0)
 
-for i in range(1,10):
-    snd = p.read()
-    if not snd:
-        print("cannot read the port")
+whole_rec = []
+SAMPLE_RATE = 48000
+RES_WAV_FILE = "/home/egor/test_numpy.wav"
 
-    print("frequency:", snd.getFrequency())
-    print("samples:", snd.getSamples())
-    print("data size:", snd.getRawDataSize())
+while True:
+    try:
+        snd = p.read()
+        if not snd:
+            print("cannot read the port")
+        num_samples = snd.getSamples()
+        num_channels = snd.getChannels()
+        res = np.zeros(shape=(num_channels, num_samples), dtype=float)
 
-p.close()
+        for ch_idx in range(num_channels):
+            for sample_id in range( num_samples ):
+                sample_value = snd.get( sample_id, 0 )
+                sample_value = np.short( sample_value )
+                dbl_value = sample_value / 65535.0
+                res[ ch_idx, sample_id] = dbl_value
+        if len(whole_rec) > 0:
+            whole_rec = np.hstack( (whole_rec, res) )
+        else:
+            whole_rec = res
+        print("1 sec is gone:{}".format(whole_rec.shape[1]))
 
-yarp.Network.fini()
+    except KeyboardInterrupt:
+        librosa.output.write_wav(RES_WAV_FILE, whole_rec, SAMPLE_RATE)
+        print("Written file")
+        p.close()
+        yarp.Network.fini()
+        sys.exit()
+
+
