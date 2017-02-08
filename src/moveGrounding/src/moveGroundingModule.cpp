90ca56fa42c3ebf3578fd2e06b5cb2877d4e666f
Massimiliano Iacono <massimiliano.iacono@iit.it>
2017-02-08 11:35:38 +0100

    added debug messages

--- src/moveGrounding/src/moveGroundingModule.cpp
+++ src/moveGrounding/src/moveGroundingModule.cpp
@@ -168,6 +168,7 @@ yarp::sig::Vector moveGroundingModule::computeNextMove(const std::vector<double>
             stateUpdated |= updateState(closestTile.i, closestTile.j, playerFlag);
         }
         i += 3;
+        yInfo() << "Closest tile = " << closestTile.i << ", " << closestTile.j;
         yInfo() << "Retrieved point " << i/3 << " = [ " << point[0] << ", " << point[1] << ", " << point[2] << "]" ;
 
     }
